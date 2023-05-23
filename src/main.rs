e66014cad8dd86e11f5b6ef82407c4ea70e08474
THEKetsu <104614980+THEKetsu@users.noreply.github.com>
2023-05-24 01:41:36 +0200

    finalisation

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@
 use actix_web::{web, App, Error, HttpResponse, HttpServer,Responder, HttpRequest,get, http::uri::Authority, };
 use serde::{Deserialize, Serialize};
 use jsonwebtoken::{encode, decode, Header, Algorithm, Validation, DecodingKey};
-
+use actix_web::HttpMessage;
+use std::fmt::Display;
 /// Our claims struct, it needs to derive `Serialize` and/or `Deserialize`
 #[derive(Debug, Serialize, Deserialize)]
 struct Claims {
@@ -13,30 +14,21 @@ struct Claims {
 
 #[get("/check_token")]
  pub async fn check_token(req:HttpRequest ) -> impl Responder {
-    println!("check token");
     let header = req.headers().get("Authorization").unwrap();
     let token_to_decode = header.to_str().unwrap();
-    // decode token
-    let decoded_token = decode::<Claims>(&token_to_decode, &DecodingKey::from_secret("bifrost".as_ref()), &Validation::default())
-    .unwrap();
-
-    let token: Claims = decoded_token.claims;
-
-    println!("{:?}",token);
-
-
-    if "true" == "true" {
+    let decoded_token = decode::<Claims>(&token_to_decode, &DecodingKey::from_secret("SECRET_KEY".as_ref()), &Validation::default())
+    .unwrap();    
+    let claim: Claims = decoded_token.claims;
+    if claim.admin == "true" {
         // retunr a HEADER Authorization with authorization
         return HttpResponse::Ok()
         .insert_header(("Authorization","True"))
-        .content_type("application/json")
-        .body("You are an admin")
+        .body("ADMIN ACCESS")
         ;
     } 
     else {
-        return HttpResponse::Ok().body("You are not an admin");
+        return HttpResponse::Ok().body("NOT ADMIN ACCESS");
     }
-
  }
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -44,7 +36,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(check_token)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("127.0.0.1", 8082))?
     .run()
     .await
 }
